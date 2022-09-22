beb47eec0e79b050484d831f403532679864c2b4
deepak <deepak@gmail.com>
2022-09-22 18:58:02 +0530

    Finished

---
+++ app/src/main/java/com/deepak/groceryapp/GroceryViewModal.kt
@@ -0,0 +1,29 @@
+package com.deepak.groceryapp
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class GroceryViewModal(private val repository: GroceryRepository) : ViewModel() {
+    fun insert(items: GroceryItems) = GlobalScope.launch {
+        repository.insert(items)
+    }
+    fun delete(items: GroceryItems) = GlobalScope.launch {
+        repository.delete(items)
+    }
+    fun getAllGroceryItems() = repository.getAllItems()
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
